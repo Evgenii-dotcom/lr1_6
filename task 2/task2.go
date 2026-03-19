be47b9236a09f9b184575f7bc206532a1e23f64e
Jeka <evgenii.guliaev04@gmail.com>
2026-03-19 16:23:51 +0300

    refactor: restructure HTTP server and background worker for better testability

--- task 2/task2.go
+++ task 2/task2.go
@@ -1,29 +1,80 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
 	"time"
 )
 
-func backgroundWorker() {
-	for {
-		fmt.Println("Фоновая обработка...")
-		time.Sleep(5 * time.Second)
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(addr string) *Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloHandler)
+
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		},
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go server")
+func (s *Server) Start() error {
+	log.Printf("HTTP сервер запущен на %s\n", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Hello from Go server"))
+}
+
+type Worker struct {
+	interval time.Duration
+	stop     chan struct{}
+}
+
+func NewWorker(interval time.Duration) *Worker {
+	return &Worker{
+		interval: interval,
+		stop:     make(chan struct{}),
+	}
+}
+
+func (w *Worker) Start() {
+	go func() {
+		ticker := time.NewTicker(w.interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("Фоновая обработка...")
+			case <-w.stop:
+				return
+			}
+		}
+	}()
+}
+
+func (w *Worker) Stop() {
+	close(w.stop)
 }
 
 func main() {
+	worker := NewWorker(5 * time.Second)
+	worker.Start()
 
-	go backgroundWorker()
+	server := NewServer(":8080")
 
-	http.HandleFunc("/", handler)
-
-	fmt.Println("HTTP сервер запущен на порту 8080")
-
-	http.ListenAndServe(":8080", nil)
+	if err := server.Start(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
+	}
 }
